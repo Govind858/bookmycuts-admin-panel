79f764401fc10a8b6c13ba9dea1565592c319131
Govind Jayakumar <govindjayakumar858@gmail.com>
2026-04-28 12:13:08 +0530

    changed baseurl

--- src/Axios/Axios.tsx
+++ src/Axios/Axios.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL: 'https://bookmycutsapp-1s3p.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://bookmycutsapp-1s3p.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
